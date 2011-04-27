8bc603f21e9498f013c4d854c41d63aeddd02a1a
jansuchy119 <jansuchy119@2b03182e-2f2c-0129-2fb0-35af7913c939>
2011-04-27 17:22:42 +0000

    nahodene PT ... datamodel ...

--- trunk/pneditor2/src/net/matmas/pneditor/actions/DataModelAction.java
+++ trunk/pneditor2/src/net/matmas/pneditor/actions/DataModelAction.java
@@ -6,6 +6,10 @@
 package net.matmas.pneditor.actions;
 
 import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+import kohary.datamodel.creator.home.MainDataModelFrame;
+import net.matmas.pnapi.properties.RoleDefinitionProperty;
+import net.matmas.pneditor.PNEditor;
 import net.matmas.util.GraphicsTools;
 
 /**
@@ -22,7 +26,12 @@ public class DataModelAction extends Action {
 	}
 
 	public void actionPerformed(ActionEvent e) {
-
+            if(PNEditor.getInstance().getDocument().getPetriNet().getProperties().getFilteredByClass(RoleDefinitionProperty.class).size()>0){
+                new MainDataModelFrame(PNEditor.getInstance().getDocument().getPetriNet());
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Create role !!!");
+            }
 	}
 
 
--- trunk/pneditor2/src/net/matmas/pneditor/actions/PTInvariantAction.java
+++ trunk/pneditor2/src/net/matmas/pneditor/actions/PTInvariantAction.java
@@ -7,23 +7,25 @@ package net.matmas.pneditor.actions;
 
 import java.awt.event.ActionEvent;
 import javax.swing.KeyStroke;
+import kohary.invariant.SMAlghoritm;
+import net.matmas.pneditor.PNEditor;
 import net.matmas.util.GraphicsTools;
 
 /**
  *
  * @author JONNY
  */
-public class PInvariantAction extends Action {
+public class PTInvariantAction extends Action {
 
-	public PInvariantAction() {
-		String name = "P Invariants";
+	public PTInvariantAction() {
+		String name = "P/T Invariants";
 		putValue(NAME, name);
-		putValue(SMALL_ICON, GraphicsTools.getIcon("pneditor/Pinv.png"));
+		putValue(SMALL_ICON, GraphicsTools.getIcon("pneditor/pti.gif"));
 		putValue(SHORT_DESCRIPTION, name);
 	}
 
 	public void actionPerformed(ActionEvent e) {
-
+                SMAlghoritm smalghoritm = new SMAlghoritm(PNEditor.getInstance().getDocument().getPetriNet(), null);
 	}
 
 
