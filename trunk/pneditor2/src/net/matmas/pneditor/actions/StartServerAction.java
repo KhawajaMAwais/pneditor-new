784154e74031e683230cb561cd866d4c24fca529
svestka@svestka.sk <svestka@svestka.sk@2b03182e-2f2c-0129-2fb0-35af7913c939>
2011-05-12 19:17:46 +0000

    start/stop server pre linux, huraaa

--- trunk/pneditor2/src/net/matmas/pneditor/actions/StartServerAction.java
+++ trunk/pneditor2/src/net/matmas/pneditor/actions/StartServerAction.java
@@ -6,11 +6,13 @@
 package net.matmas.pneditor.actions;
 
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import javax.swing.JOptionPane;
 import net.matmas.pneditor.PNEditor;
 import net.matmas.util.GraphicsTools;
+import org.GNOME.Accessibility.__LoginHelperStub;
 
 /**
  *
@@ -29,21 +31,36 @@ public class StartServerAction extends Action {
 
 
             String win_cmd = "cmd start /c \"cd tomcat/bin & startup.bat\"";
-            String unix_cmd = "./tomcat/bin/startup.sh";
+            String unix_cmd = "./startup.sh";
             String cmd = unix_cmd;
             if (System.getProperty("os.name").indexOf("Windows") >= 0)
                 cmd = win_cmd;
             Process binaryProc;
             try{
-                binaryProc = Runtime.getRuntime().exec(cmd);
+                if (System.getProperty("os.name").indexOf("Windows") >= 0)
+                    binaryProc = Runtime.getRuntime().exec(cmd);
+                else
+                    binaryProc = Runtime.getRuntime().exec(cmd, null, new File(this.getJarFolder() + "./tomcat/bin"));
 
             }catch (/*IOException*/Exception ee) {
+                System.out.println(ee.getMessage());
                 JOptionPane.showMessageDialog(PNEditor.getInstance().getMainFrame(), "Unable to run server!");
                 return;
             }
 
+            JOptionPane.showMessageDialog(PNEditor.getInstance().getMainFrame(), "Server started!");
 
 	}
+
+    private String getJarFolder() {
+        String name = this.getClass().getName().replace('.', '/');
+        String s = this.getClass().getResource("/" + name + ".class").toString();
+        s = s.replace('/', File.separatorChar);
+        s = s.substring(0, s.indexOf(".jar")+4);
+        s = s.substring(s.lastIndexOf(':')-1);
+        s = s.substring(s.indexOf(':')+1);
+        return s.substring(0, s.lastIndexOf(File.separatorChar)+1);
+    }
 
 
 }
--- trunk/pneditor2/src/net/matmas/pneditor/actions/StopServerAction.java
+++ trunk/pneditor2/src/net/matmas/pneditor/actions/StopServerAction.java
@@ -6,6 +6,7 @@
 package net.matmas.pneditor.actions;
 
 import java.awt.event.ActionEvent;
+import java.io.File;
 import javax.swing.JOptionPane;
 import net.matmas.pneditor.PNEditor;
 import net.matmas.util.GraphicsTools;
@@ -26,21 +27,35 @@ public class StopServerAction extends Action {
 	public void actionPerformed(ActionEvent e) {
 
             String win_cmd = "cmd start /c \"cd tomcat/bin & shutdown.bat\"";
-            String unix_cmd = "./tomcat/bin/shutdown.sh";
+            String unix_cmd = "./shutdown.sh";
             String cmd = unix_cmd;
             if (System.getProperty("os.name").indexOf("Windows") >= 0)
                 cmd = win_cmd;
             Process binaryProc;
             try{
-                binaryProc = Runtime.getRuntime().exec(cmd);
+                if (System.getProperty("os.name").indexOf("Windows") >= 0)
+                    binaryProc = Runtime.getRuntime().exec(cmd);
+                else
+                    binaryProc = Runtime.getRuntime().exec(cmd, null, new File(this.getJarFolder() + "./tomcat/bin"));
 
             }catch (/*IOException*/Exception ee) {
                 JOptionPane.showMessageDialog(PNEditor.getInstance().getMainFrame(), "Unable to stop server!");
                 return;
             }
 
+            JOptionPane.showMessageDialog(PNEditor.getInstance().getMainFrame(), "Server stoped.");
+
 
 	}
 
+    private String getJarFolder() {
+        String name = this.getClass().getName().replace('.', '/');
+        String s = this.getClass().getResource("/" + name + ".class").toString();
+        s = s.replace('/', File.separatorChar);
+        s = s.substring(0, s.indexOf(".jar")+4);
+        s = s.substring(s.lastIndexOf(':')-1);
+        s = s.substring(s.indexOf(':')+1);
+        return s.substring(0, s.lastIndexOf(File.separatorChar)+1);
+    }
 
 }
